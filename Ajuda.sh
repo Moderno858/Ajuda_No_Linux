bbbeb7080f39eeb4ce0b72bf357915f504997182
Moderno <71466911+Moderno858@users.noreply.github.com>
2020-12-06 16:39:23 -0300

    Update Ajuda.sh

--- Ajuda.sh
+++ Ajuda.sh
@@ -2,5 +2,24 @@
 echo "============================="
 echo "=     Ajuda no Linux        ="
 echo "============================="
-echo "Obrigado Senhor(a)$USER Por estar acessando O Informaçao Do pc" 
+echo "Obrigado Senhor(a)$USER Por esta acessando ajuda do linux" 
 
+main()
+ {
+echo "1-Atualizar"
+echo "2-Limpar lixeira"
+echo "3-Arruma bug dpkg"
+read opcao;
+   case $opcao in
+  "1")
+   Atualizar
+     ;;
+   "2")
+   Limpar_lixeira
+     ;;
+   "3")
+    Arruma_bug_dpkg
+     ;;
+ esac
+}
+main
