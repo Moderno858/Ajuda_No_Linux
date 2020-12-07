64d2eb3032a8985ddfc91566cf03dc57e3f0e136
Moderno <71466911+Moderno858@users.noreply.github.com>
2020-12-07 07:48:48 -0300

    Update Ajuda.sh

--- Ajuda.sh
+++ Ajuda.sh
@@ -3,30 +3,58 @@ echo "============================="
 echo "=     Ajuda no Linux        ="
 echo "============================="
 echo "Obrigado Senhor(a)$USER Por esta acessando ajuda do linux" 
-
 main()
  {
-echo "1-Atualizar"
-echo "2-Limpar lixeira"
-echo "3-Arruma bug dpkg"
+echo "Escolha uma opçao para você ver sobre o seu pc"
+echo "1- Atualizar"
+echo "2- Corrigio erro dpkg: error: parsing file /var/lib/dpkg/updates/0014"
+echo "3- Corrigir /var/lib/dpkg/lock"
 read opcao;
    case $opcao in
-  "1")
-   Atualizar
+   "1")
+     Atualizar
      ;;
    "2")
-   Limpar_lixeira
+  Dpkg104
      ;;
    "3")
-    Arruma_bug_dpkg
-     ;;
- esac
- Atualizar()
+    DpkgLock
+    ;;
+esac
+}
+Atualizar()
 {
+sudo apt-get update && sudo apt-get upgrade
+clear
+echo "Pronto."
 }
- Limpar_lixeira()
-{
-}
-}
-main
 
+Dpkg104()
+{
+sudo rm /var/lib/dpkg/updates/0014
+clear
+sudo apt-get clean
+clear
+sudo apt-get update
+clear
+sudo dpkg --configure -a
+clear
+echo "Pronto."
+}
+DpkgLock()
+{
+sudo rm -rf /var/lib/apt/lists/lock
+clear
+sudo apt-get update
+clear
+sudo dpkg --configure -a
+clear
+sudo apt-get -f install
+clear
+echo "Pronto."
+}
+
+
+
+
+main
